e1f83db3415d427588aa72de9727de482669e43b
grilled-swampert <128573732+grilled-swampert@users.noreply.github.com>
2024-09-04 22:43:07 +0530

    home route added

--- women-safety/src/App.jsx
+++ women-safety/src/App.jsx
@@ -1,31 +1,26 @@
-import { useState } from "react";
-// import { BrowserRouter, Routes } from "react-router-dom";
-// import { Route } from "react-router-dom";
+import { BrowserRouter, Routes } from "react-router-dom";
+import { Route } from "react-router-dom";
+import Home from "./pages/Home";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <h1>Hello</h1>
+      <BrowserRouter>
+        <Routes>
+          {/* <Route path="/" element={<Home />} /> */}
+          {/* <Route path="/:id/home" element={<Header />} />
+              <Route path="/:id/heatmap" element={<Header />} />
+              <Route path="/:id/map" element={<Header />} />
+              <Route path="/:id/logs" element={<Header />} /> */}
+          <Route path="/home" element={<Home />} />
+          {/* <Route path="/heatmap" element={<Header />} />
+          <Route path="/map" element={<Header />} />
+          <Route path="/logs" element={<Header />} />  */}
+        </Routes>
+      </BrowserRouter>
     </>
-
-
-
-      // <BrowserRouter>
-      //   <Routes>
-      //     {/* <Route path="/" element={<Header />} /> */}
-      //     {/* <Route path="/:id/home" element={<Header />} />
-      //         <Route path="/:id/heatmap" element={<Header />} />
-      //         <Route path="/:id/map" element={<Header />} />
-      //         <Route path="/:id/logs" element={<Header />} /> */}
-      //     {/* <Route path="/home" element={<Header />} />
-      //     <Route path="/heatmap" element={<Header />} />
-      //     <Route path="/map" element={<Header />} />
-      //     <Route path="/logs" element={<Header />} /> */}
-      //   </Routes>
-      // </BrowserRouter>
   );
 }
 
