43926a732afacd74c577b61d95a868a27cdce0c9
Ojas <ojaschatur18@gmail.com>
2024-09-05 17:48:28 +0530

    kuch toh commit

--- women-safety/src/pages/CCTV.jsx
+++ women-safety/src/pages/CCTV.jsx
@@ -4,6 +4,10 @@ export default function CCTV() {
     return (
         <div>
             <Navbar />
+            <div className="cctvText">
+                <h1>LIVE CCTV FOOTAGE</h1>
+                <p>This heatmap visually represents the distribution and frequency of crimes against women across different states of India, with darker areas indicating higher crime rates. By providing real-time insights, it helps identify hotspots and trends to enhance safety measures and law enforcement response.</p>
+            </div>
 
             
         </div>
