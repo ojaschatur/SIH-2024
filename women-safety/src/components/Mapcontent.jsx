bbf319029fe69a7dd768d8c670c8041bd9774583
TEJK11 <133685673+TEJK11@users.noreply.github.com>
2024-09-05 16:43:28 +0530

    Add files via upload

---
+++ women-safety/src/components/Mapcontent.jsx
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+import "./mapcontent.css";
+
+import cctvFootage from "../photos/cctvFootage.svg";
+import LeafletMap from "./Leaflet";
+
+export default function MapContent() {
+  const [locationInfo, setLocationInfo] = useState(null);
+
+  const handleLocationSelected = (location) => {
+    setLocationInfo(location);
+  };
+
+  return (
+    <div className="container">
+      <h2>Map:</h2>
+      <div className="map-container">
+        <div className="map-image">
+          <LeafletMap onLocationSelected={handleLocationSelected} />
+        </div>
+      </div>
+      <div className="cctv-container">
+        <div className="cctv-information">
+          <div className="cctv-image">
+            <img src={cctvFootage} alt="CCTV Footage" />
+          </div>
+          <div className="cctv-info">
+            <h3>CCTV Footage</h3>
+            <p className="cctv-location">
+              <strong>{locationInfo?.text || "No location selected"}</strong>
+            </p>
+            <p className="cctv-imp-info">
+              Important information about the location:
+            </p>
+            <p>
+              Number of people: <strong>{locationInfo?.people || 4}</strong>
+            </p>
+            <p>
+              Time:{" "}
+              <strong>
+                {new Date().toLocaleTimeString([], {
+                  hour: "2-digit",
+                  minute: "2-digit",
+                })}
+              </strong>
+            </p>
+            <p>
+              Gesture: {" "}
+              <strong>{locationInfo?.gesture || "Standing"}</strong>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
