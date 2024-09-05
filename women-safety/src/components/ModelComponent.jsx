f5f37a12c6a936ecd6c7357de56746331d61c4af
TEJK11 <133685673+TEJK11@users.noreply.github.com>
2024-09-06 01:21:07 +0530

    huh

--- women-safety/src/components/ModelComponent.jsx
+++ women-safety/src/components/ModelComponent.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
+import './ModelComponent.css';
 
 const VideoFeed = () => {
     return (
         <div>
-            <h1>Gender Classification Video Feed</h1>
+            <h1 class="Model-component-h1"><span>Live CCTV </span> Footage</h1>
+            <p class="Model-component-p">This heatmap visually represents the distribution and frequency of crimes against women across different states of India, with darker areas indicating higher crime rates. By providing real-time insights, it helps identify hotspots and trends to enhance safety measures and law enforcement response.y</p>
             {/* Video stream */}
             <img
                 src="http://localhost:5000/video_feed"
                 alt="Video Feed"
-                style={{ width: '100px', height: 'auto', border: '2px solid #000' }}
             />
         </div>
     );
